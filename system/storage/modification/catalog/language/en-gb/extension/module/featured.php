d94a39884538e7d5f79e29c21af397e028cbe76a
user.mail <Michael_Costa99@outlook.com>
2021-05-10 20:59:52 +0200

    uploading file

---
+++ system/storage/modification/catalog/language/en-gb/extension/module/featured.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title'] =  'Featured products' ;
+
+// Text
+
+				$_['text_option']              = 'Available Options';
+				$_['text_select']              = '--- Please Select ---';
+				
+ $_['text_quick'] = 'Details'; 
+$_['text_tax']      = 'Ex Tax:';
